574ec83dd7e968ee5a47bcdf54823dba0577ac93
gimminjae <min356812@naver.com>
2023-05-05 00:00:03 +0900

    create SpringMVC-1 sevlet project

---
+++ SpringMVC-1/sevlet/src/main/java/com/sevlet/SevletApplication.java
@@ -0,0 +1,13 @@
+package com.sevlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SevletApplication.class, args);
+	}
+
+}
---
+++ SpringMVC-1/sevlet/src/test/java/com/sevlet/SevletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
