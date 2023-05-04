4a027df32a32e919936af00ffd82a41e05ff598c
gimminjae <min356812@naver.com>
2023-05-05 00:34:25 +0900

    Hello Servlet

---
+++ SpringMVC-1/servlet/src/main/java/com/servlet/basic/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello") //name -> Servlet Name, urlPattern -> Url Mapping
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+        
+        String username = request.getParameter("username"); //RequestParam
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain"); //response's content type
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write("hello " + username);
+        
+    }
+}
