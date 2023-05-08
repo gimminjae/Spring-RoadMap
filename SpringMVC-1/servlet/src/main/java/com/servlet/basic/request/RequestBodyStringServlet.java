258b3597bf9a0ac8c924e77d2d641b21f84ace5e
gimminjae <min356812@naver.com>
2023-05-08 22:25:29 +0900

    Http Request Data - body: TEXT

---
+++ SpringMVC-1/servlet/src/main/java/com/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,31 @@
+package com.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        response.getWriter().write("ok");
+    }
+}
+/**
+ * POST http://localhost:8080/request-body-string
+ * content-type: text/plain
+ * message body: hello
+ * 결과: messageBody = hello
+ */
