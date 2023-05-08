08029f9841024b03a52b71bee2109e834b396f2c
gimminjae <min356812@naver.com>
2023-05-08 23:52:22 +0900

    HttpServletResponse - JSON

---
+++ SpringMVC-1/servlet/src/main/java/com/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,29 @@
+package com.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.servlet.basic.HelloData;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+    private ObjectMapper objectMapper = new ObjectMapper();
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8"); //actually don't need
+
+        //{ "username": "kim", "age": 20 }
+        HelloData helloData = new HelloData();
+        helloData.setUsername("kim");
+        helloData.setAge(20);
+
+        String result = objectMapper.writeValueAsString(helloData);
+        response.getWriter().write(result);
+    }
+}
