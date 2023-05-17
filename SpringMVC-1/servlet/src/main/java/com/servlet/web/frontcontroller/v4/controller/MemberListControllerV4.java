070460c9498267c02da24125d1274c3a83d56361
gimminjae <min356812@naver.com>
2023-05-18 00:21:21 +0900

    FrontController:simple and useful Controller - v4

---
+++ SpringMVC-1/servlet/src/main/java/com/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
@@ -0,0 +1,19 @@
+package com.servlet.web.frontcontroller.v4.controller;
+
+import com.servlet.domain.member.Member;
+import com.servlet.domain.member.MemberRepository;
+import com.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV4 implements ControllerV4 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        List<Member> members = memberRepository.findAll();
+
+        model.put("members", members);
+        return "members";
+    }
+}
