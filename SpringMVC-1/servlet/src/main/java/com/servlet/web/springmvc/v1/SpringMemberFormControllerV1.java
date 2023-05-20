7350935ddf3fe97fcf2f4c21cbe763dfec8d1199
gimminjae <min356812@naver.com>
2023-05-20 17:42:57 +0900

    Spring MVC - Start

---
+++ SpringMVC-1/servlet/src/main/java/com/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,18 @@
+package com.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SpringMemberFormControllerV1 {
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+}
+/**
+ * @Controller: Registered as Spring Bean automatically.
+ * @RequestMapping: Map Request Information.
+ * @ModelAndView: Return Model and View information.
+ */
