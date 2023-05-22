7827fe4662406a56415d305ef86a98ff50a2c0d4
gimminjae <min356812@naver.com>
2023-05-23 08:59:36 +0900

    Http Response - static resource

---
+++ SpringMVC-1/springmvc/src/main/java/hello/springmvc/basic/response/ResponseStaticController.java
@@ -0,0 +1,13 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ResponseStaticController {
+
+    /**
+     * static resource's path: src/main/resources/static
+     * If file is in src/main/resources/static/basic/hello-form.html, url is http://localhost:8080/basic/hello-form.html
+     */
+}
