ed31a5378d5290769fee5a7beed023dfc7cbaaac
gimminjae <min356812@naver.com>
2023-05-22 21:22:21 +0900

    Http Request Parameter - @RequestParam

--- SpringMVC-1/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ SpringMVC-1/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -21,5 +22,64 @@ public class RequestParamController {
         int age = Integer.parseInt(request.getParameter("age"));
         log.info("username={}, age={}", username, age);
         response.getWriter().write("ok");
+    }
+
+    /**
+     * Usage Of @RequestParam
+     * Binding with Parameter Name
+     * @RequestParam("username") String memberName == String memberName = request.getParameter("username")
+     */
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(@RequestParam("username") String memberName,
+                                 @RequestParam("age") int memberAge) {
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+
+    /**
+     * If Http Parameter Name is same as variable name, '("username")' can be omitted
+     */
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * If simple data type such as String and int, @RequestParam can be omitted too.
+     */
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * Attribute 'required'
+     * Default value of @RequestParam's attribute 'required' is 'true'
+     * If parameter username is empty, 400 error occurs
+     * Danger! : if url is '/request-param-required?username=', username is "", empty string
+     * -> username = "" ok!!!!
+     */
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * Attribute 'defaultValue'
+     * If parameter is null or "", Default value is assigned
+     */
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
     }
 }
