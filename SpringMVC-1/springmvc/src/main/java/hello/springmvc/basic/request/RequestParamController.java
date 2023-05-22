7b52146b8600b889d9b9810ccc69832b16c0bbb6
gimminjae <min356812@naver.com>
2023-05-22 21:14:55 +0900

    Http Request Parameter - Query Parameter, Html Form

---
+++ SpringMVC-1/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,25 @@
+package hello.springmvc.basic.request;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@Slf4j
+@RestController
+public class RequestParamController {
+    /**
+     * http://localhost:8080/request-param-v1?username=hello&age=20
+     * Usage Of HttpServletRequest
+     */
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        log.info("username={}, age={}", username, age);
+        response.getWriter().write("ok");
+    }
+}
