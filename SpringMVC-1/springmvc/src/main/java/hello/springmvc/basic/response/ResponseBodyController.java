1018b32f817fb0a46a7d1979a69db51784892ec3
gimminjae <min356812@naver.com>
2023-05-23 09:31:53 +0900

    Http Response - Write Directly to Http API, Body

---
+++ SpringMVC-1/springmvc/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -0,0 +1,72 @@
+package hello.springmvc.basic.response;
+
+import hello.springmvc.basic.HelloData;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class ResponseBodyController {
+
+    /**
+     * Deliver ok Response Message at Http Message Body using HttpServletResponse Object.
+     */
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletResponse response) throws IOException {
+        response.getWriter().write("ok");
+    }
+
+    /**
+     * ResponseEntity extends HttpEntity.
+     * HttpEntity have Http Message Header and Body.
+     * Additionally, ResponseEntity hava Http Response Code( 200, 400, 500...)
+     */
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String> responseBodyV2() {
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+    /**
+     * Not using View, you can write directly Http Message through Http Message Converter.
+     * ResponseEntity works similarly.
+     */
+    @ResponseBody
+    @GetMapping("/response-body-string-v3")
+    public String responseBodyV3() {
+        return "ok";
+    }
+
+    /**
+     * Return ResponseEntity.
+     * Data is returned, being converted into Json through Http Message Converter.
+     */
+    @GetMapping("/response-body-json-v1")
+    public ResponseEntity<HelloData> responseBodyJsonV1() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+
+        return new ResponseEntity<>(helloData, HttpStatus.OK);
+    }
+
+    /**
+     * @ResponseStatus: Setting Http Response Code(200, 400, 500...)
+     */
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @GetMapping("/response-body-json-v2")
+    public HelloData responseBodyJsonV2() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+        return helloData;
+    }
+}
