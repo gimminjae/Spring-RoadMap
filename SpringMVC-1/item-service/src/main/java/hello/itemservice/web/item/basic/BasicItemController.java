aba21300a26ae66ee9802716bac6710acc397ec9
gimminjae <min356812@naver.com>
2023-05-25 14:32:33 +0900

    Implement Item List Page: Usage of thymeleaf

---
+++ SpringMVC-1/item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -0,0 +1,34 @@
+package hello.itemservice.web.item.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+    private final ItemRepository itemRepository;
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+    /**
+     * Add Data For Test
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("testA", 10000, 10));
+        itemRepository.save(new Item("testB", 20000, 20));
+    }
+}
