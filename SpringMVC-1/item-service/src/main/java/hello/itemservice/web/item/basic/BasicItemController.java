31c47512b6debdba90769e97328cf5d4e0f278be
gimminjae <min356812@naver.com>
2023-05-29 14:24:12 +0900

    Implement edit Item Page

--- SpringMVC-1/item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ SpringMVC-1/item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -42,6 +42,12 @@ public class BasicItemController {
         model.addAttribute("item", item); //When use @ModelAttribute, this can be omitted. Automatically, object(@ModelAttribute) delivered at Model.
         return "basic/item";
     }
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
     /**
      * Add Data For Test
      */
