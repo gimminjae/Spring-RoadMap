3c4e135733a6057e31519f88285fe6dfa571f547
gimminjae <min356812@naver.com>
2023-05-29 14:11:16 +0900

    Implement add Item Mapping (@RequestParam or @ModelAttribute)

--- SpringMVC-1/item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ SpringMVC-1/item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -6,10 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,6 +30,17 @@ public class BasicItemController {
     @GetMapping("/add")
     public String addForm() {
         return "basic/addForm";
+    }
+    @PostMapping("/add")
+    public String addItemV1(@ModelAttribute Item item, //@RequestParam String itemName, @RequestParam int price, @RequestParam Integer quantity,
+                            Model model) {
+//        Item item = new Item();
+//        item.setItemName(itemName);
+//        item.setPrice(price);
+//        item.setQuantity(quantity);
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+        return "basic/item";
     }
     /**
      * Add Data For Test
