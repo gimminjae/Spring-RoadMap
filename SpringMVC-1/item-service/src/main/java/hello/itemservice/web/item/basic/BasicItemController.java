50fc4c8d0cb7046a6a8542edabf239a184ef4827
gimminjae <min356812@naver.com>
2023-05-29 14:26:51 +0900

    Implement edit Item Mapping

--- SpringMVC-1/item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ SpringMVC-1/item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -48,6 +48,11 @@ public class BasicItemController {
         model.addAttribute("item", item);
         return "basic/editForm";
     }
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
     /**
      * Add Data For Test
      */
