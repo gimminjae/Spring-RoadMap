4bc270cd17725313847a67b06e1cbee1d056bc96
gimminjae <min356812@naver.com>
2023-06-03 11:41:53 +0900

    Injection dependencies through constructor and AppConfig - Member

---
+++ Spring Core Basic/core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+/**
+ * AppConfig
+ * 
+ * AppConfig creates object implements interface and connects the objects to config total work way of application.
+ */
+public class AppConfig {
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(
+                new MemoryMemberRepository(),
+                new FixDiscountPolicy());
+    }
+}
--- Spring Core Basic/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ Spring Core Basic/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,7 +1,16 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    /**
+     * Now, MemberServiceImpl don't depend on the object implements MemberRepository interface.
+     * Which objects are injected through constructor is decided by AppConfig.
+     * MemberServiceImpl only depends on MemberRepository interface, in this way, the DIP principle is upheld.
+     */
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     public void join(Member member) {
         memberRepository.save(member);
