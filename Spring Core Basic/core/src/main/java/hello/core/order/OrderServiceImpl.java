8d5a369ee21b4522ada93fc20cc530d94a3077c5
gimminjae <min356812@naver.com>
2023-06-03 09:18:05 +0900

    Problems with applying a new discount policy(OCP DIP)

--- Spring Core Basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring Core Basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,7 +8,16 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); //Fix discount
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); //Rate discount
+    /**
+     * When change discount policy, have to modify code of OrderService.
+     * We separated role and implementaion.
+     * But We failed to follow the OCP, DIP principles.
+     * DIP: OrderServiceImpl depends on both interface and implemented class.
+     * OCP: When feature is extended and modified, it affects to client code.
+     */
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
