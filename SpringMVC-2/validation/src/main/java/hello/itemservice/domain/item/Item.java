dba866720824c84d021efbd8e4a03032f33b6da7
gimminjae <min356812@naver.com>
2024-01-29 23:40:06 +0900

    Bean Validation - Object error

--- SpringMVC-2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ SpringMVC-2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,18 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000")
+/**
+ * 실제 사용해보면 제약이 많고 복잡하다. 그리고 실무에서는 검증 기능이 해당 객체의 범위를 넘어서는 경우들도 종종 등장하는데, 그런 경우 대응이 어렵다.
+ * 따라서 오브젝트 오류(글로벌 오류)의 경우 @ScriptAssert 을 억지로 사용하는 것 보다는 오브젝트 오류 관련 부분만 직접 자바 코드로 작성하는 것을 권장한다.
+ */
 public class Item {
 
     private Long id;
