7a73512c55c28e97706eb3d490072eef9ce51ae2
gimminjae <min356812@naver.com>
2024-01-30 22:25:55 +0900

    Bean Validation - 수정에도 적용

--- SpringMVC-2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ SpringMVC-2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -81,7 +81,20 @@ public class ValidationItemControllerV3 {
     }
 
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000,
+                        resultPrice}, null);
+            }
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v3/editForm";
+        }
+
         itemRepository.update(itemId, item);
         return "redirect:/validation/v3/items/{itemId}";
     }
